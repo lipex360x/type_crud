d2668a993edf14fd02cc7947a591bbe2ebdabb79
Felipe Borges <lipex360@gmail.com>
2021-12-21 01:50:43 -0300

    jest updated

--- jest.config.js
+++ jest.config.js
@@ -5,21 +5,11 @@ module.exports = {
   bail: true,
   clearMocks: true,
   collectCoverage: true,
-
-  // collectCoverageFrom: [
-  //   '<rootDir>/src/modules/**/useCases/**/**/*.ts'
-  //   '<rootDir>/src/shared/middlewares/**/useCases/**/*.ts}'
-  // ],
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/node_modules/'],
-
-  coverageReporters: [
-    'text-summary',
-    'lcov'
-  ],
-
+  coverageReporters: ['text-summary','lcov'],
+  
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
-
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
